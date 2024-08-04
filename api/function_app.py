6050a5b50787085239d827621ad120572e0431bd
ShadNygren <shad@hipster.net>
2024-08-03 19:31:47 -0700

    Remove obsolete Python code

--- api/function_app.py
+++ api/function_app.py
@@ -15,45 +15,6 @@ def message(req: func.HttpRequest) -> func.HttpResponse:
         mimetype="application/json"
     )
 
-#    name = req.params.get('name')
-#    if not name:
-#        try:
-#            req_body = req.get_json()
-#        except ValueError:
-#            pass
-#        else:
-#            name = req_body.get('name')
-#
-#    if name:
-#        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-#    else:
-#        return func.HttpResponse(
-#             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-#             status_code=200
-#        )
-
-@app.route(route="upload_file_old", auth_level=func.AuthLevel.ANONYMOUS)
-def upload_file_old(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
-
-
 @app.route(route="upload_file", auth_level=func.AuthLevel.ANONYMOUS)
 def upload_file(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -81,10 +42,6 @@ def upload_file(req: func.HttpRequest) -> func.HttpResponse:
         logging.error(f"Error during file upload: {str(e)}")
         return func.HttpResponse(f"File upload failed: {str(e)}", status_code=500)
 
-#def process_file(file_content):
-#    # Stub for additional functionality to process the uploaded file
-#    logging.info('Processing file...')
-#    # Add your file processing logic here
 
 
 import os
